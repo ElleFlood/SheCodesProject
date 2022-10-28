32d4091c65aa151c0905ba538c615af522858d6f
ElleFlood <eleanor.t.flood@gmail.com>
2022-10-28 08:55:06 +0100

    added temp

--- src/index.js
+++ src/index.js
@@ -30,14 +30,19 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
-  document.querySelector("#temperature").innerHTML = response.data.name;
+}
+
+function displayTemperature(response) {
   console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.main.temp;
 }
 
 function searchCity(city) {
   let apiKey = "34d8821390d535d13d1044d3142143f9";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
+  axios.get(apiURL).then(displayTemperature);
 }
 
 function pressSubmit(event) {
